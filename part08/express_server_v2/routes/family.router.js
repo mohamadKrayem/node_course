15d540994da5dbddee9cf12f8bd4197d512ccc1a
Mohamad Krayem <mk2242682@gmail.com>
2023-01-30 19:43:54 +0200

    Add the router functionality to express_server_v2

--- part08/express_server_v2/routes/family.router.js
+++ part08/express_server_v2/routes/family.router.js
@@ -4,11 +4,13 @@ const family = require('../controllers/family.controller');
 
 const familyRouter = express.Router();
 
-//callbacks are in the controller because they controll the req and res;
+//this middleware works only if the server passes through /family so it passes through the familyRouter
 familyRouter.use(function(req, res, next) {
    console.log("from the familyRouter: ", req.ip);
    next();
 });
+
+//callbacks are in the controller because they controll the req and res;
 familyRouter.get("/", family.getAllFamily);
 familyRouter.post("/", family.postFamilyMember);
 familyRouter.get("/:id", family.getSingleFamilyMember);
--- part08/express_server_v2/server.js
+++ part08/express_server_v2/server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const server = express();
 const PORT = 3000;
 const messages = require("./controllers/messages.controller");
+
+//we group the routes the belong to /family in one router
 const familyRouter = require('./routes/family.router');
 
 server.use(express.json());
-server.use('/family', familyRouter)
+server.use('/family', familyRouter)//go to family.router.js
 
+//callbacks are in the controller because they controll the req and res;
 server.get("/messages", messages.getMessages);
 server.get("/messages/:number", messages.getUpdatedMessage);
 
