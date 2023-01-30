1e7415687a74cb7149635f360ac388a04d5dadc8
Mohamad Krayem <mk2242682@gmail.com>
2023-01-30 22:46:56 +0200

    Add the functionality of responding by HTML and CSS files using express.static() function

--- part08/express_server_v2/server.js
+++ part08/express_server_v2/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require("path");
+
 const server = express();
 const PORT = 3000;
 const messagesRouter = require("./routes/messages.router");
@@ -7,6 +9,7 @@ const messagesRouter = require("./routes/messages.router");
 const familyRouter = require('./routes/family.router');
 
 server.use(express.json());
+server.use('/site', express.static(path.join(__dirname, "public")));
 server.use('/family', familyRouter)//go to family.router.js
 
 server.use("/messages", messagesRouter);
